50f5b8931eb6456e6f2fa2e4cf23d2dc255edeed
AjaiRup <ajairuparelia@gmail.com>
2025-09-30 21:36:17 +0000

    Deploying to gh-pages from @ AjaiRup/ajairup.github.io@0ffcf8344f090559000b559ae11de87645be698b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AjaiRup/ajairuparelia.github.io",
+    "data-repo": "AjaiRup/ajairup.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
